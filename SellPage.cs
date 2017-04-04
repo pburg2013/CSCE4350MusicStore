96e57e2019e7cc68e3f260db285a45d8db07dd1f
pburg2013 <pburg2013@gmail.com>
2017-04-04 09:52:08 -0500

    Update SellPage.cs
    updated Sell page

--- SellPage.cs
+++ SellPage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,17 +22,24 @@ namespace MusicStore
         string query { get; set; }
         string username { get; set; }
         int userID { get; set; }
+        string prodName { get; set; }
+        string prodQuality { get; set; }
+        string prodDescription { get; set; }
+        string prodCat { get; set; }
+        string prodSubCat { get; set; }
+        double prodPrice { get; set; }
+        int prodQuantity { get; set; }
         MySqlConnection myConnection { get; set; }
         MySqlCommand commandDatabase { get; set; }
         MySqlDataReader reader { get; set; }
-        public SellPage(string name, int id)
+        public SellPage(string name, int userid)
         {
             InitializeComponent();
             string myConnString = "datasource=127.0.0.1;port=3307;username=root;password=password;database=musicstore;";
             myConnection = new MySqlConnection(myConnString);
             //call a funciton to populate the comboboxes
             username = name;
-            userID = id;
+            userID = userid;
             populateList();
             populateBoxes();
 
@@ -127,7 +134,179 @@ namespace MusicStore
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //user has added a new product to sell in the store
 
+            //getting the strings and saving them 
+
+            prodName = NameBox.Text;
+            prodPrice = Convert.ToDouble(PriceBox.Text);
+            prodQuantity = Convert.ToInt32(QuantityBox.Text);
+            prodDescription = DescBox.Text;
+            int CatID = get_categoryID();
+            int QID = get_qualityID();
+            //add item to the items table
+            int itemID = add_item(CatID, QID);
+
+
+            //add the item in the inventory table
+
+            query = "INSERT INTO TABLE inventory (ItemID, Quantity) VALUES (@itemID, @prodQuantity); ";
+            try
+            {
+                myConnection.Open();
+
+                using (commandDatabase = new MySqlCommand(query, myConnection))
+                {
+                    commandDatabase.Parameters.Add("@itemID", MySqlDbType.Int32).Value = itemID;
+                    commandDatabase.Parameters.Add("@prodQuantity", MySqlDbType.Int32).Value = prodQuantity;
+                 
+                    int x = commandDatabase.ExecuteNonQuery();
+                    if (x > 0)
+                    {
+                        textBox2.Text = "Product added to store inventory!";
+                    }
+                    else
+                    {
+                        textBox2.Text = "Product unable to be added to inventory";
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex.Message);
+            }
+
+
+            myConnection.Close();
+
+        }
+
+        private void QualityBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox qbox = (ComboBox)sender;
+            prodQuality = (string) qbox.SelectedItem;
+            int index = 0; 
+            for(int i =0; i < qualityNames.Count(); i++){
+                if (qualityNames[i] == prodQuality)
+                    index = i;
+            }
+            QualityTipText.Text = prodQuality + ": " + qualityDescriptions[index];
+        }
+
+        private void CategoryBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cbox = (ComboBox)sender;
+            prodCat = (string)cbox.SelectedItem;
+
+        }
+
+        private void SubCategoryBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cbox = (ComboBox)sender;
+            prodSubCat = (string)cbox.SelectedItem;
+        }
+        public int get_categoryID()
+        {
+            query = "select i.CategoryID from category as i, category as g where i.CategoryName like '%" + prodSubCat + "%' and g.CategoryName like '%" + prodCat + "%' and i.ParentID = g.CategoryID";
+            myConnection.Open();
+            commandDatabase = new MySqlCommand(query, myConnection);
+            commandDatabase.CommandTimeout = 60;
+            reader = commandDatabase.ExecuteReader();
+            int catID = -1;
+
+            //if the reader has rows then save and return the id
+            if (reader.HasRows)
+            {
+                reader.Read();
+                catID = reader.GetInt32(0);
+
+            }
+            myConnection.Close();
+            return catID;
+
+
+
+        }
+        public int add_item(int CatID, int QualID)
+        {
+            query = "INSERT INTO item (ItemName, Description, SellerID, CategoryID,QualityID,Price) VALUES ( @prodName,@prodDescription,@userID,@CatID, @QualID,@prodPrice );";
+            try
+            {
+                myConnection.Open();
+                using (commandDatabase = new MySqlCommand(query, myConnection))
+                {
+                    commandDatabase.Parameters.Add("@prodName", MySqlDbType.VarChar).Value = prodName;
+                    commandDatabase.Parameters.Add("@prodDescription", MySqlDbType.VarChar).Value = prodDescription;
+                    commandDatabase.Parameters.Add("@userID", MySqlDbType.Int32).Value = userID;
+                    commandDatabase.Parameters.Add("@CatID", MySqlDbType.Int32).Value = CatID;
+                    commandDatabase.Parameters.Add("@QualID", MySqlDbType.Int32).Value = QualID;
+                    commandDatabase.Parameters.Add("@prodPrice", MySqlDbType.Decimal).Value = prodPrice;
+                    int x = commandDatabase.ExecuteNonQuery();
+                    if (x > 0)
+                    {
+                        MessageBox.Show( "Product created! Now adding to Inventory");
+                    }
+                    else
+                    {
+                        textBox2.Text = "Product creation unsuccessful..";
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex.Message);
+            }
+
+
+
+            myConnection.Close();
+            //get and return the new items ID
+            query = "select ItemID from item where ItemName like '%" + prodName + "%' and SellerID = " + userID + " ;";
+            myConnection.Open();
+            commandDatabase = new MySqlCommand(query, myConnection);
+            commandDatabase.CommandTimeout = 60;
+            reader = commandDatabase.ExecuteReader();
+            int itemID = -1;
+
+            //if the reader has rows then save and return the id
+            if (reader.HasRows)
+            {
+                reader.Read();
+                itemID = reader.GetInt32(0);
+
+            }
+            myConnection.Close();
+            return itemID;
+
+        }
+   
+        public int get_qualityID()
+        {
+            query = "select qualityID from quality where qualityName = '%" + prodQuality + "%'";
+            myConnection.Open();
+            commandDatabase = new MySqlCommand(query, myConnection);
+            commandDatabase.CommandTimeout = 60;
+            reader = commandDatabase.ExecuteReader();
+            int QID = -1;
+
+            //if the reader has rows then save and return the id
+            if (reader.HasRows)
+            {
+                reader.Read();
+                QID = reader.GetInt32(0);
+
+            }
+            myConnection.Close();
+            return QID;
+        }
     }
    
 }
